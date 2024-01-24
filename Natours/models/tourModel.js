f3a8b5325bcb6fdba6d52c7b8475935965aa3278
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-01-24 04:54:13 +0200

    +add Validators

--- Natours/models/tourModel.js
+++ Natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,7 +9,11 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name "], // Validator
       unique: true,
       trim: true,
+      maxlength: [40, "A tour name must have less than or equal 40 characters"], // built in Validator
+      minlength: [10, "A tour name must have more than or equal 10 characters"], // built in Validator
+      // validate: [validator.isAlpha, "Tour name must only contain characters "],
     },
+
     slug: String,
 
     duration: {
@@ -22,10 +27,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"], // (Validator)  values that are allowed to use in this field
+        message: "Difficulty is either : easy, medium, difficult", // error message
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"], // built in Validator
+      max: [5, "Rating must be below 5.0"], // built in Validator
     },
     ratingsQuantity: {
       type: Number,
@@ -35,7 +46,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      // custom validator
+      validate: {
+        validator: function (val) {
+          return val < this.price; // this keyword refers to the current document when creating new document and don`t work with update
+        },
+        message: "Discount price({VALUE}) must be below the regular price", // ({VALUE}) -> used to access the value that input ed (val)
+      },
+    },
     summary: {
       type: String,
       trim: true,
