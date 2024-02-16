7857cb5e5187f01b34f2dc347a43e992607fead3
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-02-16 07:44:14 +0200

    print process env

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./Natours/config.env" });
 const app = require("./app");
 // console.log(app.get("env"));
-
 const DB = process.env.DATABASE;
 
 // console.log(DB);
