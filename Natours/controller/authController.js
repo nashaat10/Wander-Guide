acc75016a3be005384e497409313762de8e71289
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-02-18 18:57:23 +0200

    add token

--- Natours/controller/authController.js
+++ Natours/controller/authController.js
@@ -1,11 +1,23 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // Create a token for the user and send it in the response body along with the user data in the response body as well
+  token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
