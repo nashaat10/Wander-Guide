d593d9d0cff0bd7c8770a854ac3772aa12b3cefa
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-03-15 16:29:37 +0200

    +

--- app.js
+++ app.js
@@ -1,33 +1,67 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit"); // limit the number of request from a single ip
+const helmet = require("helmet"); // set security http headers
+const mongoSanitize = require("express-mongo-sanitize"); // data sanitization against NoSQL query injection
+const xss = require("xss-clean"); // data sanitization against xss
+const hpp = require("hpp"); // prevent parameter pollution
 
 const AppError = require("./Natours/utils/appError");
 const globalErrorHandler = require("./Natours/controller/errorController");
 const tourRouter = require("./Natours/routes/tourRoutes");
 const userRouter = require("./Natours/routes/userRoutes");
 
-// 1) Middleware
+// 1) Global Middleware
+
+// set security http headers
+app.use(helmet());
+
+// body parser, reading data from body into req.body
 app.use(express.json());
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against xss
+app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+// development logging
 app.use(morgan("dev"));
-// if (process.env.NODE_ENV === "development") {
-//   app.use(morgan("dev"));
-// }
+
+// serving static files
 app.use(express.static("./Natours/public"));
 
+// limit the number of request from a single ip
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour!",
+});
+
+// limit the number of request from a single ip
+app.use("/api", limiter);
+
+//test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);
   next();
 });
-
-// 2) Route Handler
-
-// app.delete("/api/v1/tours/:id", deleteTour);
-// app.get("/api/v1/tours", getAllTours);
-// app.get("/api/v1/tours/:id", getTour);
-// app.post("/api/v1/tours", createTour);
-// app.patch("/api/v1/tours/:id", updateTour);
 
 // 3) Routes
 
