2aabd9000efd59529641cacbf347fd34c96a0e8c
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-05-08 06:28:20 +0300

    +add indexes to prevent duplicate reviews

--- Natours/models/reviewModel.js
+++ Natours/models/reviewModel.js
@@ -34,6 +34,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// to prevent duplicate reviews from the same user on the same tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: "tour",
