25a553a7fc90ba6da63aa318b88846e3a1e06e87
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-06-23 17:41:41 +0300

    remove send email

--- Natours/controller/authController.js
+++ Natours/controller/authController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-const sendEmail = require("../utils/email");
+// const Email = require("../utils/email");
 const crypto = require("crypto");
 
 const signToken = (id) =>
@@ -37,14 +37,12 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-    role: req.body.role,
-  });
+  const newUser = await User.create(req.body);
 
+  const url = `${req.protocol}://${req.get("host")}/me`;
+
+  console.log(url);
+  // await new Email(newUser, url).sendWelcome();
   // Create a token for the user and send it in the response body along with the user data in the response body as well
   createSendToken(newUser, 201, res);
 });
@@ -143,11 +141,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   const message = `Forgot your password? Submit a patch request with your new password and passwordConfirm to: ${resetURl}.\nIf you didn't forget your password, please ignore this email!`;
   try {
-    await sendEmail({
-      email: user.email,
-      subject: "Your password reset token (valid for 10 min)",
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: "Your password reset token (valid for 10 min)",
+    //   message,
+    // });
 
     res.status(200).json({
       status: "success",
--- Natours/utils/email.js
+++ Natours/utils/email.js
@@ -1,27 +1,51 @@
-const nodeMailer = require("nodemailer");
+// const nodeMailer = require("nodemailer");
+// const pug = require("pug");
+// const htmlToText = require("html-to-text");
 
-const sendEmail = async (options) => {
-  // 1) create a transporter
-  const transporter = nodeMailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+// module.exports = class Email {
+//   constructor(user, url) {
+//     this.to = user.email;
+//     this.firstName = user.name.split(" ")[0];
+//     this.url = url;
+//     this.from = `Mohamed Nashaat <${process.env.EMAIL_FROM}>`;
+//   }
+//   newTransport() {
+//     if (process.env.NODE_ENV === "production") {
+//       return 1;
+//     }
+//     return nodeMailer.createTransport({
+//       host: process.env.EMAIL_HOST,
+//       port: process.env.EMAIL_PORT,
+//       auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD,
+//       },
+//     });
+//   }
 
-  // 2) define the email options
+//   async send(template, subject) {
+//     // 1) render HTML based on a pug template
+//     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+//       firstName: this.firstName,
+//       url: this.url,
+//       subject,
+//     });
 
-  const mailOptions = {
-    from: "Mohamed Nashaat <hello@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+//     // 2) define the email options
 
-  // 3) actually send the email
-  await transporter.sendMail(mailOptions);
-};
+//     const mailOptions = {
+//       from: this.from,
+//       to: this.to,
+//       subject,
+//       html,
+//       text: htmlToText.fromString(html),
+//     };
 
-module.exports = sendEmail;
+//     // 3) actually send the email
+//     await this.newTransport().sendMail(mailOptions);
+//   }
+
+//   async sendWelcome() {
+//     await this.send("welcome", "Welcome to the Natours Family!");
+//   }
+// };
