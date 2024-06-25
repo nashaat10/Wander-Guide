e0d59c190e39d94d4cedc10d58a8dafea42f0838
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-06-25 22:53:46 +0300

    add stripe routes and controller

---
+++ Natours/controller/bookingController.js
@@ -0,0 +1,42 @@
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
+const Tour = require("../models/tourModel");
+const factory = require("./handlerFactory");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  //1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+
+  //2) Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+          },
+        },
+        quantity: 1,
+      },
+    ],
+
+    mode: "payment",
+    success_url: `${req.protocol}://${req.get("host")}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+  });
+
+  //3) Send it to the client
+
+  res.status(200).json({
+    status: "success",
+    session,
+  });
+});
--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require("./Natours/controller/errorController");
 const tourRouter = require("./Natours/routes/tourRoutes");
 const userRouter = require("./Natours/routes/userRoutes");
 const reviewRouter = require("./Natours/routes/reviewRoutes");
+const bookingRouter = require("./Natours/routes/bookingRoutes");
 
 // 1) Global Middlewares
 // set security http headers
@@ -69,6 +70,7 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/bookings", bookingRouter);
 
 // all is used to handel all the http methods in one time
 app.all("*", (req, res, next) => {
