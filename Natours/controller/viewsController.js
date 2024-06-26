c9819ece56e3a26f3d045ec003a45975cbe247ad
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-06-26 20:37:51 +0300

    add views controller

---
+++ Natours/controller/viewsController.js
@@ -0,0 +1,73 @@
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const User = require("../models/userModel");
+const Booking = require("../models/bookingModel");
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+  // 3) Render that template using tour data from 1)
+  res.status(200).render("overview", {
+    title: "All Tours",
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get tour data include(Guides, Reviews)
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: "reviews",
+    select: "review rating user",
+  });
+
+  if (!tour) {
+    return next(new AppError("There is no tour with that name.", 404));
+  }
+
+  res.status(200).render("tour", {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render("login", {
+    title: "Log into your account",
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render("account", {
+    title: "Your account",
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const { name, email } = req.body;
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { name: name, email: email },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).render("account", {
+    title: "Your account",
+    user: updatedUser,
+  });
+});
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  const bookings = await Booking.find({ user: req.user.id });
+
+  const tourIds = bookings.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIds } });
+
+  res.status(200).render("overview", {
+    title: "My Bookings",
+    tours,
+  });
+});
