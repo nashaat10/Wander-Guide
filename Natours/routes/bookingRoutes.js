b9a26d4c9748f9b242443208b6016568d4bb5a8a
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-06-28 05:36:50 +0300

    add checkout route

--- Natours/routes/bookingRoutes.js
+++ Natours/routes/bookingRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const bookingController = require("../controller/bookingController");
 const authController = require("../controller/authController");
 
-const router = express.Router({});
+const router = express.Router();
 
+router.get("/create-checkout", bookingController.createBookingCheckout);
 router.use(authController.protect);
 
 router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
